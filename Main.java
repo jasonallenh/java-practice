58b07eedd18e74e37117fd51aac3297d2b121002
jasonallenh <jasonallenh@gmail.com>
2019-04-08 18:48:58 -0400

    added string exercise to main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,10 @@ class Main {
     String s2 = "stringtwo";
     String s3 = s1 + s2 + "LIT";
 
-    System.out.println(s3.toUpperCase());
+    s3 =  s3.toUpperCase(); //permanently changes s3 to upper case
+
+    System.out.println(s3.replace("G","o")); //replace is formatted replace(new char,old char)
+    System.out.println(s3);
 
   }
 }
