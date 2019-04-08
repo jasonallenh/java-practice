c73949031e65d26cc9ea1f54013f727a6aa81500
jasonallenh <jasonallenh@gmail.com>
2019-04-08 18:13:24 -0400

    adding repl.it java file

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    char c = 'c';
+    String s1 = "stringone";
+    String s2 = "stringtwo";
+    String s3 = s1 + s2 + "LIT";
+    System.out.println(s3);
+
+  }
+}
