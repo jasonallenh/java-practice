d8cdadcb3e59e2091194a361e9246f47ff92de46
jasonallenh <jasonallenh@gmail.com>
2019-04-09 16:53:02 -0400

    Completed "for Loops" lesson

---
+++ forloops.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    /*int i = 1;
+    while(i <= 100)
+    {
+      System.out.println(i);
+      i++;
+    }*/
+
+//the commented out code above is what is replaced by the for loop below
+
+    for(int i = 1; i <=100; i++)
+    {
+      System.out.println(i);
+    }
+  }
+}
